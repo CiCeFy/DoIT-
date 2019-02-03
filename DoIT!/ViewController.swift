f454cc06b32b3cbd26c814b77cddf63a22bc1e13
Chris <Chris@carolins-macbook-pro.local>
2019-02-03 12:00:25 +0100

    Initial Commit

---
+++ DoIT!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoIT!
+//
+//  Created by Chris on 03.02.19.
+//  Copyright © 2019 CiCeFy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
